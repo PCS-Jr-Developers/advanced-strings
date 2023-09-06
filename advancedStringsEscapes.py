192b3f4983f0ef604571e6e2685d3f893c908d9f
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-09-06 18:36:04 -0400

    Create advancedStringsEscapes.py

---
+++ advancedStringsEscapes.py
@@ -0,0 +1,13 @@
+single_quoted = 'she said, \'Hello!\''
+double_quoted = "He said, \"Hi!\""
+print(single_quoted)
+print(double_quoted)
+
+multi_line = "Line 1\nLine 2"
+print(multi_line)
+
+indented = "Indented\tText"
+print(indented)
+
+path = "C:\\Users\\Username\\Documents"
+print(path)
