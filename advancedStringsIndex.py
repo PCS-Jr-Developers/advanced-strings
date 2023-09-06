b0b5e73fc7f88b4c530098b4723738c2d91e5efc
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-09-06 18:39:27 -0400

    Create advancedStringsIndex.py

---
+++ advancedStringsIndex.py
@@ -0,0 +1,5 @@
+message = "Hello, World!"
+first_character= message[0] #Gets the first character "H"
+second_character= message[2] #Gets the second character "e"
+print(first_character)
+print(second_character)
