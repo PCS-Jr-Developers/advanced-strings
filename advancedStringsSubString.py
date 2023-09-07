4f94b329dacb3fb20717ff8124e42e10019b79a2
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-09-07 01:21:23 -0400

    Create advancedStringsSubString.py

---
+++ advancedStringsSubString.py
@@ -0,0 +1,8 @@
+# Define a string
+original_string = "Hello, World!"
+
+# Extract a substring from index 7 to 11 (inclusive of 7, exclusive of 12)
+substring = original_string[7:12]
+
+# Print the substring
+print(substring)
